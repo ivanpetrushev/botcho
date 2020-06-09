151d9373048418a605a48302b559c1bf00780173
Ivan Petrushev <ivanatora@gmail.com>
2020-06-09 14:47:01 +0300

    init

---
+++ main.py
@@ -0,0 +1,74 @@
+import discord
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import random
+import time
+import json
+
+token = open("token.txt", "r").read()
+
+
+def get_water():
+    url = 'https://api.gfycat.com/v1/gfycats/search?search_text=drink+water'
+    fp = urlopen(url)
+    contents = json.load(fp)
+    print(contents)
+    item = random.choice(contents['gfycats'])
+    item_url = item['max2mbGif']
+    return item_url
+
+
+def scrape_sub(sub_name):
+    out = []
+    print('Scraping sub', sub_name)
+    time.sleep(2)
+    url = 'https://old.reddit.com/r/' + sub_name + '/'
+    fp = urlopen(url)
+    contents = fp.read()
+    soup = BeautifulSoup(contents, "html.parser")
+
+    for item in soup.select('div[data-author-fullname]'):
+        print('item', item.attrs['data-url'])
+        img_url = item.attrs['data-url']
+        if 'i.redd.it' in img_url or 'imgur' in img_url:
+            out.append(img_url)
+    return out
+
+
+def load_pool():
+    out = []
+    subs = ['funny', 'ProgrammerHumor']
+    for sub in subs:
+        for item in scrape_sub(sub):
+            out.append(item)
+    random.shuffle(out)
+    print(f'loading pool with {len(out)} items:', out)
+    return out
+
+
+class MyClient(discord.Client):
+    link_pool = []
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        # self.link_pool = load_pool()
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        print(message)
+        if message.content.startswith('!fun'):
+            if len(self.link_pool) == 0:
+                self.link_pool = load_pool()
+            my_msg = self.link_pool.pop()
+            await message.channel.send(my_msg)
+        if message.content.startswith('!test'):
+            await message.channel.send('https://i.redd.it/b61u4gk752351.jpg')
+        if message.content.startswith('!water'):
+            gfycat_url = get_water()
+            await message.channel.send("Пийте вода! :)")
+            await message.channel.send(gfycat_url)
+
+
+token = open("token.txt", "r").read()
+client = MyClient()
+client.run(token)
